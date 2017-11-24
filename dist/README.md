57759d70fabb422f4fddd5565680bc2afd1c00f6
Fabian Orthen <fabian@Fabians-MacBook-Pro-2.local>
2017-11-24 16:01:55 +0100

    dist

---
+++ dist/README.md
@@ -0,0 +1,1 @@
+# SSBR-Kirby
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
-
-var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+
+// Tasks below this line
+
+gulp.task('hello', function() {
+    console.log('Hello Zell');
+  });
+
+
+gulp.task('sass', function(){
+    return gulp.src('app/assets/css/*.scss')
+      .pipe(sass()) // Using gulp-sass
+      .pipe(gulp.dest('dist/assets/css'));
+  });
